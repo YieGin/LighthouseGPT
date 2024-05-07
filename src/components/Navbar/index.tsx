62fc8f2f23e5f6d6f22459849e335b349b43fbe0
YieGin <pumasami7@gmail.com>
2024-05-07 15:05:29 +0100

    added Navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import React from 'react';
+
+interface IconContainerProps {
+  iconSrc: string;
+  alt: string;
+}
+
+const Navbar: React.FC = () => {
+  return (
+    <div className='bg-white px-[41px] flex items-center h-[101px] justify-between w-full'>
+      <h2 className='text-[#343C6A] font-semibold text-[28px]'>Free-LighthouseGPT</h2>
+      <div className='md:flex hidden gap-[30px]'>
+        <IconContainer iconSrc="/icons/settings.svg" alt='settings icon' />
+        <IconContainer iconSrc="/icons/notification.svg" alt='notification icon' />
+        <Image
+          className='object-cover rounded-full cursor-pointer'
+          width={60}
+          height={60}
+          src="/images/profile.png"
+          alt="Profile"
+        />
+      </div>
+    </div>
+  );
+};
+
+const IconContainer = ({ iconSrc, alt }: IconContainerProps) => (
+  <div className='rounded-full w-[50px] h-[50px] bg-[#F5F7FA] flex items-center justify-center cursor-pointer'>
+    <Image width={25} height={25} src={iconSrc} alt={alt} />
+  </div>
+);
+
+export default Navbar;
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sidebar = () => {
+  return <div className="h-screen w-[10%] border-r-[1px]">yo</div>;
+};
+
+export default Sidebar;
